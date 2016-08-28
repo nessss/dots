eab9a45ec41e95bd32bf9576c213a5ad48fd1f8d
Ness Morris <karplusstrong@gmail.com>
2016-08-28 02:33:14 -0700

    added install shell script

---
+++ install.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+if [ ! $TARGET ]; then
+    # TARGET=$HOME
+    TARGET=$HOME/dotfiletest
+fi
+
+echo "======= Homebrew ================================="
+echo "*"
+echo "*"
+
+# homebrew
+
+if ! which -s brew; then
+    echo "*  brew not installed; installing"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "*  brew installed; updating"
+    brew update
+fi
+
+echo "*"
+echo "*"
+echo "=================================================="
+echo
+echo "======= .dotfiles ================================"
+echo "*"
+echo "*"
+
+# make folder for misc files
+if [ ! -d $TARGET/.dotfiles ]; then
+    echo "*  $TARGET/.dotfiles not found; creating"
+    mkdir $TARGET/.dotfiles
+else
+    echo "*  $TARGET/.dotfiles found;"
+fi
+echo "*"
+echo "*"
+
+if [ -e $TARGET/.vimrc ] && [ ! -h $TARGET/.vimrc ]; then
+    echo "*  $TARGET/.vimrc found; moving to $TARGET/.dotfiles/old_vimrc"
+    mv $TARGET/.vimrc $TARGET/.dotfiles/old_vimrc
+    echo "*"
+fi
+
+if [ -e $TARGET/.bashrc ] && [ ! -h $TARGET/.bashrc]; then
+    echo "*  $TARGET/.bashrc found; moving to $TARGET/.dotfiles/old_bashrc"
+    mv $TARGET/.bashrc $TARGET/.dotfiles/old_bashrc
+    echo "*"
+fi
+
+if [ ! -d $TARGET/.dotfiles/bash ]; then
+    echo "*  creating symlink to bash files at $TARGET/.dotfiles/bash"
+    ln -s $(realpath ./bash) $TARGET/.dotfiles/bash
+else
+    echo "*  $TARGET/.dotfiles/bash found"
+fi
+
+echo "*"
+echo "*"
+echo "=================================================="
+echo
+echo "======= vim ======================================"
+echo "*"
+echo "*"
+
+if [ ! -e $TARGET/.vimrc ]; then
+    echo "*  creating symlink to vimrc at $TARGET/.vimrc"
+    ln -sv $(realpath ./vimrc) $TARGET/.vimrc
+fi
+echo "*"
+echo "*"
+
+# link vim bundles
+echo "*  updating submodules"
+git submodule update
+echo "*"
+echo "*"
+
+if [ ! -d $TARGET/.vim/bundle ]; then
+    echo "*  $TARGET/.vim/bundle not found; creating"
+    mkdir -p $TARGET/.vim/bundle
+else
+    echo "*  $TARGET/.vim/bundle found"
+fi
+echo "*"
+echo "*"
+
+for dir in ./vim/*/
+do
+    if [[ `greadlink -f $TARGET/.vim/bundle/$dir` != `realpath $dir` ]]; then
+        echo "*  creating symlink to $dir at $TARGET/.vim/bundle/$dir"
+        ln -s $(realpath $dir) $TARGET/.vim/bundle/
+    fi
+done
+
+echo "*  vim bundle plugins installed"
+
+echo "*"
+echo "*"
+echo "=================================================="
+echo
+echo "======= bash ====================================="
+echo "*"
+echo "*"
+
+# set up bash
+
+if [ ! -e $TARGET/.bashrc ]; then
+    echo "*  creating symlink to bashrc at $TARGET/.bashrc"
+    ln -s $(realpath ./bashrc) $TARGET/.bashrc
+fi
+
+echo "*"
+echo "*"
+echo "=================================================="
+echo
+echo
+echo "dotfiles installed and up to date"
